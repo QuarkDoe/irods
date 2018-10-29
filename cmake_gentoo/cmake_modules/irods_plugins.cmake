637082baf779bb06b6279d35cd380f55cf8511e2
Quark Doe <quarkdoe@gmail.com>
2018-10-29 04:56:56 +0000

    Добавил сборку auth и network плагинов в сборку без сервера.

--- cmake_gentoo/cmake_modules/irods_plugins.cmake
+++ cmake_gentoo/cmake_modules/irods_plugins.cmake
@@ -1,7 +1,11 @@
-add_subdirectory(plugins/api)
 add_subdirectory(plugins/auth)
-add_subdirectory(plugins/database)
-add_subdirectory(plugins/microservices)
 add_subdirectory(plugins/network)
-add_subdirectory(plugins/resources)
-add_subdirectory(plugins/rule_engines)
+
+if( BUILD_SERVER )
+	add_subdirectory(plugins/api)
+	add_subdirectory(plugins/database)
+	add_subdirectory(plugins/microservices)
+	add_subdirectory(plugins/resources)
+	add_subdirectory(plugins/rule_engines)
+endif( BUILD_SERVER )
+
